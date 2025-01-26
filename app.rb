dc82f77e0c0180cc31e5369a45ba3a7226768e21
Remy Castella <remy.jpen@gmail.com>
2025-01-26 17:22:56 +0900

    add basic features

--- app.rb
+++ app.rb
@@ -7,8 +7,22 @@ require "faker"
 Dir[File.join(__dir__, 'app', 'models', '*.rb')].each { |file| require file }
 
 set :database, { adapter: "sqlite3", database: "db/beerlib.sqlite3" }
+set :views, File.join(__dir__, "app", "views")
 
 get "/" do
-  beer = Beer.all.sample
-  beer.users.to_json
+  @beers = Beer.all.sample(10).uniq(&:name)
+  @users = User.all
+  erb :index
+end
+
+get "/users/:username" do
+  @user = User.find_by(username: params[:username])
+  favorites = Favorite.where(user: @user)
+  @favorite_beers = favorites.map { |favorite| Beer.find(favorite.beer_id.to_i) }.uniq(&:name)
+  erb :users
+end
+
+get "/beers/:beer_name" do
+  @beer = Beer.all.select { |beer| beer.name.downcase.gsub(" ", "_") == params[:beer_name] }.first
+  erb :beer
 end
